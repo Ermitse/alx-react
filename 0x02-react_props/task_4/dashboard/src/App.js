f9bf40a0884342be1a6b97997e66ae89d8ac9c97
ermitse <ermiastse@gmail.com>
2023-07-31 19:07:36 +0000

    task4

--- 0x02-react_props/task_4/dashboard/src/App.js
+++ 0x02-react_props/task_4/dashboard/src/App.js
@@ -1,6 +1,5 @@
 import logo from './Holberton-logo.jpg';
 import './App.css';
-import {getFullYear, getFooterCopy} from './utils'
 
 function App() {
   return (
@@ -11,21 +10,9 @@ function App() {
 			</div>
 			<div className='App-body'>
 				<p>Login to access the full dashboard</p>
-				<section className='form-inputs'>
-					<section className='input'>
-						<label htmlFor='email'>Email:</label>
-						<input type='email' name='email' id='email' 
-						placeholder='you@example.com'/>
-					</section>
-					<section className='input'>
-						<label htmlFor='password'>Password: </label>
-						<input type='password' name='password' id='password' />
-					</section>
-					<button>OK</button>
-				</section>
 			</div>
 			<div className='App-footer'>
-				<p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+				<p>Copyright 2020 - holberton School</p>
 			</div>
 		</div>
   );
