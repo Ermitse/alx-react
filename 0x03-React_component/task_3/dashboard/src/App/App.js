0996f0e4f64ca4fafddae8426206b0a154ac7f80
ermitse <ermiastse@gmail.com>
2023-08-02 20:07:25 +0000

    APP

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -1,21 +1,74 @@
-import logo from './Holberton-logo.jpg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
 
-function App() {
-  return (
-    <div className='App'>
-			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
-			</div>
-			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
-			</div>
-			<div className='App-footer'>
-				<p>Copyright 2020 - holberton School</p>
-			</div>
-		</div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleLogout);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleLogout);
+  }
+  handleLogout(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+  render() {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    ];
+    const { isLoggedIn } = this.props;
+    return (
+      <Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <Header />
+        {isLoggedIn ? (
+          <BodySectionWithMarginBottom title='Course list'>
+            <CourseList listCourses={listCourses} />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title='Log in to continue'>
+            <Login />
+          </BodySectionWithMarginBottom>
+        )}
+        <BodySection title='News from the School'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </BodySection>
+        <Footer />
+      </Fragment>
+    );
+  }
 }
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => undefined,
+};
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
 
 export default App;
--- 0x03-React_component/task_3/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.test.js
@@ -3,27 +3,47 @@ import React from 'react';
 import App from './App';
 
 describe('<App />', () => {
-  it('App renders without crashing', () => {
+  it('renders without crashing', () => {
     const wrapper = shallow(<App />);
   });
 
-  it('App contain Notifications component', () => {
+  it('contain Notifications component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Notifications')).toHaveLength(1);
   });
 
-  it('App contain Header component', () => {
+  it('contain Header component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Header')).toHaveLength(1);
   });
 
-  it('App contain Login component', () => {
+  it('contain Login component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Login')).toHaveLength(1);
   });
 
-  it('App contain Footer component', () => {
+  it('contain Footer component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('Footer')).toHaveLength(1);
+  });
+
+  it('CourseList', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('CourseList')).toHaveLength(0);
+  });
+
+  it('isLoggedIn true', () => {
+    const wrapper = shallow(<App isLoggedIn />);
+    expect(wrapper.find('Login')).toHaveLength(0);
+    expect(wrapper.find('CourseList')).toHaveLength(1);
+  });
+
+  it('logOut', () => {
+    const logOut = jest.fn(() => undefined);
+    const wrapper = shallow(<App logOut={logOut} />);
+    const alert = jest.spyOn(global, 'alert');
+    expect(alert);
+    expect(logOut);
+    jest.restoreAllMocks();
   });
 });
