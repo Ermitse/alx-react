2c2fad8626830ea41d85902a7ec54138a8888c8f
ermitse <ermiastse@gmail.com>
2023-08-07 17:40:57 +0000

    fix ta1

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,27 +1,39 @@
-import React from "react";
-import { shallow } from "enzyme";
-import NotificationItem from "./NotificationItem";
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import { StyleSheetTestUtils } from 'aphrodite';
 
-describe("<NotificationItem />", () => {
-  it("renders without crashing", () => {
+describe('<NotificationItem />', () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it('render without crashing', () => {
     const wrapper = shallow(<NotificationItem />);
-    shallow(<NotificationItem />);
+    expect(wrapper.exists());
   });
 
-  it("renders type and value props", () => {
-    const wrapper = shallow(<NotificationItem type="default" value="test" />);
-    const li = wrapper.find("li");
+  it('renders type and value props', () => {
+    const wrapper = shallow(<NotificationItem type='default' value='test' />);
+    const li = wrapper.find('li');
+    expect(wrapper.exists());
+    expect(li.exists());
     expect(li).toHaveLength(1);
-    expect(li.text()).toEqual("test");
-    expect(li.prop("data-notification-type")).toEqual("default");
+    expect(li.text()).toEqual('test');
+    expect(li.prop('data-notification-type')).toEqual('default');
   });
 
-  it("renders html prop", () => {
-    const text = "Here is the list of notifications";
+  it('renders html prop', () => {
+    const text = 'Here is the list of notifications';
     const wrapper = shallow(
-      <NotificationItem html={{ __html: "<u>test</u>" }} />
+      <NotificationItem html={{ __html: '<u>test</u>' }} />
     );
-    const li = wrapper.find("li");
+    const li = wrapper.find('li');
+    expect(wrapper.exists());
+    expect(li.exists());
     expect(li.html()).toEqual(
       '<li data-notification-type="default"><u>test</u></li>'
     );
